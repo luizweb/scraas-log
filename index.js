d0a879d0f9beca5e40f2dd3962cb64f78fd098a4
Luiz Antonio Guimaraes Simoes <luiz.simoes@rfb.gov.br>
2023-02-03 12:13:47 -0300

    callback

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post("/callback", async (req,res)=>{
         //tokenId --> if-none-match
         //JSON.stringify(req.body)
         const tokenId = "6341c6cc-b12d-4c85-a682-270531fc20ce"
-        const tokenId2 = req.header("if-none-match")
+        const tokenId2 = req.header("if-none-match").replace('"','')
         const updateLog = await logModel.findOneAndUpdate({tokenId: tokenId}, {resposta: tokenId2, status:1}, {new: true, runValidators: true})
         return res.status(200).json(updateLog);
     } catch (error) {
